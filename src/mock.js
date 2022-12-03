fbe3f212c39cf79d6632e6ec12a0b167eb3814fc
shailgautam4ever <rayskcoc@gmail.com>
2022-12-03 13:23:33 +0530

    added: mock questions data

---
+++ src/mock.js
@@ -0,0 +1,14 @@
+export const questions = [
+  {
+    question: "Who is writer of Harry Potter?",
+    options: ["Harry Kane", "Christopher Nolan", "J.K Rowling", "Tony Kakkar"]
+  },
+  {
+    question: "What command is given by Targaryens to dragon to breathe fire?",
+    options: ["Wingardium leviOsa", "Dracarys", "Abracadabra", "Winter is coming"]
+  },
+  {
+    question: "Who is referred as Desi Dumbledore in Hustle 2.0",
+    options: ["GD47", "Jonny", "Chota Don", "Jaadugar"]
+  }
+]
