7e6c3b2f0198cf524b435cca30109e265be379a7
shailgautam4ever <rayskcoc@gmail.com>
2022-12-03 13:24:53 +0530

    modified: using all the components for quiz app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { useState } from "react";
+import Result from "./components/Result";
+import AnswerSheet from "./components/AnswerSheet";
+import Questions from "./components/Questions";
+import { questions } from "./mock";
+
 
 function App() {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const [answerSheet, setAnswerSheet] = useState({})
+  const [showResult, setShowResult] = useState(false)
+
+  const handleOnNext = () => {
+    setActiveIndex((curr) => curr + 1)
+  }
+  const handleOnPrev = () => {
+    setActiveIndex((curr) => curr - 1)
+  }
+  const handleOnOptSelect = (e) => {
+    setAnswerSheet((curr) => {
+      curr[activeIndex + 1] = {
+        ans: e.target.value,
+        question: questions[activeIndex].question
+      }
+      return { ...curr }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!showResult ? (
+        <MainContainer>
+          <AnswerSheet answerSheet={answerSheet} />
+          <Questions
+            activeIndex={activeIndex}
+            answerSheet={answerSheet}
+            handleOnPrev={handleOnPrev}
+            handleOnNext={handleOnNext}
+            handleOnOptSelect={handleOnOptSelect}
+            setShowResult={setShowResult}
+          />
+        </MainContainer>
+      ) : (
+        <Result answerSheet={answerSheet} totalQuestions={questions.length} />
+      )}
+    </>
   );
 }
-
 export default App;
+
+const MainContainer = styled.div`
+  min-height:calc(100vh - 60px);
+  background-color: #eee;
+  display: flex;
+  padding: 30px;
+  gap:30px
+`
